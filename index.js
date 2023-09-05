8fa3615908fea1aad27d784dd6f1deca064df747
Miriam <118216207+cotufetes@users.noreply.github.com>
2023-09-05 19:13:37 +0200

    updated URI to connect Heroku and AtlasDB

--- index.js
+++ index.js
@@ -15,7 +15,8 @@ const Users = Models.User;
 const Genres = Models.Genre;
 const Directors = Models.Director;
 
-mongoose.connect('mongodb://localhost:27017/myFlixDB', { useNewUrlParser: true, useUnifiedTopology: true });
+//mongoose.connect('mongodb://localhost:27017/myFlixDB', { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect( process.env.CONNECTION_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
 //Uses body-parser to read body object
 app.use(bodyParser.json());
